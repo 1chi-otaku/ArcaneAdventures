1419004dafe455cf1c1b81ef51ac49f094910c20
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-11-07 02:25:52 +0000

    Fixed Light

---
+++ Arcane Adventures/Assets/Scripts/ClearInputField.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearInputField : MonoBehaviour
+{
+    public InputField inputField;
+
+    public void ClearInput()
+    {
+        if (inputField != null)
+        {
+            inputField.text = ""; // Очищаем текст в input field
+        }
+    }
+}
