970ef6ea74d43c8063617322e8899b8a8484f0ac
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-11-20 22:51:33 +0000

    Начало катсцены при входе в платформер
    Я люблю голубику.

---
+++ Arcane Adventures/Assets/Scripts/2D/EnterPlatformer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class EnterPlatformer : MonoBehaviour
+{
+    private bool cutscenePlayed = false;
+    public PlayableDirector cutsceneDirector; // ссылка на таймлайн катсцены
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (!cutscenePlayed)
+                {
+                    cutsceneDirector.stopped += OnCutsceneEnd; // подписываемся на событие окончания воспроизведения
+                    cutsceneDirector.Play();
+
+                    cutscenePlayed = true;
+                }
+            }
+        }
+    }
+
+    // Вызывается по окончании воспроизведения катсцены
+    private void OnCutsceneEnd(PlayableDirector director)
+    {
+        // Отписываемся от события, чтобы избежать многократного вызова
+        cutsceneDirector.stopped -= OnCutsceneEnd;
+
+        // Загружаем следующую сцену
+        SceneManager.LoadScene("Platformer-1");
+    }
+}
