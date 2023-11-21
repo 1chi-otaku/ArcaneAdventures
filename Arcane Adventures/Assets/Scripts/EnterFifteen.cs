077de8c1103b2d464be6c23be77c71e473866715
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-11-21 07:03:55 +0000

    15 Enter

---
+++ Arcane Adventures/Assets/Scripts/EnterFifteen.cs
@@ -0,0 +1,23 @@
+using Photon.Pun.Demo.SlotRacer;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class EnterFifteen : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                SceneManager.LoadScene("15");
+            }
+        }
+    }
+
+}
