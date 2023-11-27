f45322306e8926d67ab996a8b21ca8c2da373d4b
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-11-27 22:33:20 +0000

    Cursor Fix?

---
+++ Arcane Adventures/Assets/Scripts/FixCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCursor : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Сделать курсор видимым и разблокировать его при загрузке сцены
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void OnDestroy()
+    {
+        // При уничтожении объекта сохранить состояние курсора
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
