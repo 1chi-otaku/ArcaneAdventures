b377d044a883350828b53a2d3c7927cea2b1d79a
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-11-28 09:43:43 +0000

    1chi

---
+++ Arcane Adventures/Assets/Scripts/ObjectDisappear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectDisappear : MonoBehaviour
+{
+    public GameObject targetObject;
+    public Canvas EpromptCanvas;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                targetObject.SetActive(false);
+                EpromptCanvas.enabled = false;
+            }
+        }
+    }
+}
