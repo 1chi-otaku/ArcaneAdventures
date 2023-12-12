4225255ed2acce5995662ac6ba32870ad3bdacc4
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2023-12-12 06:18:07 +0000

    DialogBox 1

---
+++ Arcane Adventures/Assets/Scripts/DialogueSystem/DialogueBaseClass.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+
+        public bool finished { get; private set; }
+
+        protected IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, AudioClip sound, float delayBetweenCutscenes)
+        {
+            //textHolder.color = textColor;
+            textHolder.font = textFont;
+            Debug.Log("Text Color: " + textColor.ToString());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                textHolder.text += input[i];
+                SoundManager.instance.PlaySound(sound);
+                yield return new WaitForSeconds(delay);
+            }
+            //yield return new WaitForSeconds(delayBetweenCutscenes);
+            yield return new WaitUntil(() => Input.GetMouseButton(0));
+            finished = true;
+        }
+    }
+}
